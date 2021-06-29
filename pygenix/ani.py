d8e4fae9fbcb6693ea9278c1cd2ccaa985c4b306
DinelMax <73960425+lightning283@users.noreply.github.com>
2021-06-29 18:12:38 +0530

    Add files via upload

---
+++ pygenix/ani.py
@@ -0,0 +1,19 @@
+import sys,time
+import os
+def animatedtext(text):
+    message = text
+    def animation(message):
+        for char in message:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.1)
+    animation(message)
+
+def animatedtextfile(file):
+    message = open(file, "r")
+    def animation(message):
+        for char in message:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.1)
+    animation(message) 
