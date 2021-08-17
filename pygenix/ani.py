b3f08e5f2e8fb9ba40d07dca7ce84cb3f39193be
lightning283 <deadtarget283@gmail.com>
2021-08-17 18:19:58 +0530

    new python tkinter hover btn

--- pygenix/ani.py
+++ pygenix/ani.py
@@ -16,4 +16,18 @@ def animatedtextfile(file , time_sleep):
             sys.stdout.write(char)
             sys.stdout.flush()
             time.sleep(time_sleep)
-    animation(message) 
+    animation(message)
+def tk_hover_btn(root,x,y,text,bcolor,fcolor , cmd):
+    #taken from --> https://www.youtube.com/watch?v=u8Em9OQJXaI
+    def on_enter(e):
+        mybutton['background'] = bcolor
+        mybutton['foreground'] = fcolor
+
+    def on_leave(e):
+        mybutton['background'] = fcolor
+        mybutton['foreground'] = bcolor
+
+    mybutton = Button(root,width=22 , height = 2 , text = text , fg = bcolor , bg = fcolor , border = 0 , activeforeground=fcolor , activebackground = bcolor , command = cmd)
+    mybutton.bind("<Enter>" , on_enter)
+    mybutton.bind("<Leave>" , on_leave)
+    mybutton.place(x=x , y=y)
